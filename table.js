26c8b02d5dd5e5197d264305d3ba151c13a66781
cham-s <cattouma@student.42.fr>
2017-05-28 15:56:09 +0200

    table done

--- table.js
+++ table.js
@@ -1,3 +1,5 @@
+var MOUNTAINS = require("./mountains");
+
 function rowHeights(rows) {
     return rows.map(function(row) {
         return row.reduce(function(max, cell) {
@@ -23,4 +25,115 @@ function drawTable(rows) {
             return block[lineNo];
         }).join(" ");
     }
+
+    function drawRow(row, rowNum) {
+        var blocks = row.map(function (cell, colIndex) {
+            return cell.draw(widths[colIndex], heights[rowNum]);
+        });
+
+        return blocks[0].map(function (_, lineNo) {
+            return drawLine(blocks, lineNo);
+        }).join("\n");
+    }
+
+    return rows.map(drawRow).join("\n");
 }
+
+function repeat(string, times) {
+    var result = "";
+    for (var i = 0; i < times; i++) {
+        result += string;
+    }
+    return result;
+}
+
+function TextCell(text) {
+    this.text = text.split("\n");
+}
+
+TextCell.prototype.minWidth = function() {
+    return this.text.reduce(function(width, line) {
+        return Math.max(width, line.length);
+    }, 0);
+};
+
+TextCell.prototype.minHeight = function() {
+    return this.text.length;
+};
+
+TextCell.prototype.draw = function(width, height) {
+    var result = [];
+    for (var i = 0; i < height; i++) {
+        var line = this.text[i] || "";
+        result.push(line + repeat(" ", width - line.length));
+    }
+    return result;
+};
+
+function UnderlinedCell(inner) {
+    this.inner = inner;
+}
+
+UnderlinedCell.prototype.minWidth = function() {
+    return this.inner.minWidth();
+};
+
+UnderlinedCell.prototype.minHeight = function() {
+    return this.inner.minHeight() + 1;
+};
+
+UnderlinedCell.prototype.draw = function(width, height) {
+    return this.inner.draw(width, height -1)
+    .concat([repeat("-", width)]);
+};
+
+function RTextCell(text) {
+    TextCell.call(this, text);
+}
+
+RTextCell.prototype = Object.create(TextCell.prototype);
+
+RTextCell.prototype.draw = function(width, height) {
+    var result = [];
+    for (var i = 0; i < height; i++) {
+        var line = this.text[i] || "";
+        result.push(repeat(" ", width - line.length) + line);
+    }
+    return result;
+};
+
+var rows = [];
+for (var i = 0; i < 5; i++) {
+    var row = [];
+    for (var j = 0; j < 5; j++) {
+        if ((j + i) % 2 == 0)
+            row.push(new TextCell("##"));
+        else
+            row.push(new TextCell("  "));
+    }
+    rows.push(row);
+}
+
+function dataTable(data) {
+    var keys = Object.keys(data[0]);
+    var headears = keys.map(function(name){
+        return new UnderlinedCell(new TextCell(name));
+    });
+    
+    var body = data.map(function(row) {
+        return keys.map(function(name) {
+            var value = row[name];
+            if (typeof value == "number")
+                return  new RTextCell(String(value));
+            else
+                return  new TextCell(String(value));            
+        });
+    });
+
+    var res = [headears].concat(body);
+    return res;
+}
+
+//console.log(drawTable(dataTable(MOUNTAINS)));
+
+
